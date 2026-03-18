ba7d3e22a3a23d966b4f24d0068d7cadb62bde92
yjj <yichengyjj@qq.com>
2026-03-18 12:02:06 +0800

    重新设置add

--- build/CMakeFiles/copy_compile_commands.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_compile_commands"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_compile_commands.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/testRaft.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for testRaft.
-# This may be replaced when dependencies are built.
